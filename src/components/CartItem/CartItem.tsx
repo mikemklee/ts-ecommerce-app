3ea7f1a3f075652bba4998bd53a161b4a39bf9cc
Mike Lee <mikemunkyu.lee@gmail.com>
2020-03-29 12:54:18 -0400

    Added CartItem component to display inside CartPreview

---
+++ src/components/CartItem/CartItem.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './CartItem.styles.scss';
+import { CartItem as CartItemType } from '../../redux/cart/cart.reducer';
+
+type Props = {
+  item: CartItemType;
+};
+
+const CartItem = ({ item: { imageUrl, price, name, quantity } }: Props) => (
+  <div className="cart-item">
+    <img src={imageUrl} alt="item" />
+    <div className="item-details">
+      <span className="name">{name}</span>
+      <span className="price">
+        {quantity} x ${price}
+      </span>
+    </div>
+  </div>
+);
+
+export default CartItem;
--- src/components/CartPreview/CartPreview.tsx
+++ src/components/CartPreview/CartPreview.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import './CartPreview.styles.scss';
-import CustomButton from '../CustomButton/CustomButton';
 
-const CartPreview = () => (
+import CartItem from '../CartItem/CartItem';
+import CustomButton from '../CustomButton/CustomButton';
+import { RootState } from '../../redux/rootReducer';
+
+type Props = ReturnType<typeof mapStateToProps>;
+
+const CartPreview = ({ cartItems }: Props) => (
   <div className="cart-preview">
-    <div className="cart-items"></div>
+    <div className="cart-items">
+      {cartItems.map(cartItem => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
+    </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
-export default CartPreview;
+const mapStateToProps = (state: RootState) => ({
+  cartItems: state.cart.items,
+});
+
+export default connect(mapStateToProps)(CartPreview);
