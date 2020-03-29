2c4c35df36517459b1264275c149093eb2630026
Mike Lee <mikemunkyu.lee@gmail.com>
2020-03-29 13:01:37 -0400

    Display count of items in cart in CartIcon component

--- src/components/CartIcon/CartIcon.tsx
+++ src/components/CartIcon/CartIcon.tsx
@@ -4,20 +4,25 @@ import { connect } from 'react-redux';
 import './CartIcon.styles.scss';
 import icon from '../../assets/images/shopping-cart.png';
 
-import { RootActionTypes } from '../../redux/rootReducer';
+import { RootActionTypes, RootState } from '../../redux/rootReducer';
 import { toggleCartVisible } from '../../redux/cart/cart.actions';
 
-type Props = ReturnType<typeof mapDispatchToProps>;
+type Props = ReturnType<typeof mapStateToProps> &
+  ReturnType<typeof mapDispatchToProps>;
 
-const CartIcon = ({ toggleCartVisible }: Props) => (
+const CartIcon = ({ itemCount, toggleCartVisible }: Props) => (
   <div className="cart-icon" onClick={toggleCartVisible}>
     <img src={icon} alt="cart" />
-    <span>0</span>
+    <span>{itemCount}</span>
   </div>
 );
+
+const mapStateToProps = (state: RootState) => ({
+  itemCount: state.cart.items.reduce((acc, item) => acc + item.quantity, 0),
+});
 
 const mapDispatchToProps = (dispatch: Dispatch<RootActionTypes>) => ({
   toggleCartVisible: () => dispatch(toggleCartVisible()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
